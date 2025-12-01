0d5b8f0f107c3dd19e58811d3581667f05d62a6d
L4zKa <kaiserlutz24@gmail.com>
2025-12-01 23:05:47 +0100

    Update base and icon paths for Secret Santa app

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/secret-santa/",   
   plugins: [
     react(),
     VitePWA({
@@ -10,18 +11,18 @@ export default defineConfig({
       manifest: {
         name: "Secret Santa",
         short_name: "SecretSanta",
-        start_url: "/",
+        start_url: "/secret-santa/",     
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/santaIcon.png",
+            src: "/secret-santa/santaIcon.png", 
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/santaIcon.png",
+            src: "/secret-santa/santaIcon.png",
             sizes: "512x512",
             type: "image/png",
           },
