7ff38a2c24a0d9b998a58b882bfee26530f419c5
L4zKa <kaiserlutz24@gmail.com>
2025-12-01 23:08:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/secret-santa/",   
+  base: "/secret-santa/", // REQUIRED
   plugins: [
     react(),
     VitePWA({
@@ -11,18 +11,18 @@ export default defineConfig({
       manifest: {
         name: "Secret Santa",
         short_name: "SecretSanta",
-        start_url: "/secret-santa/",     
+        start_url: "/secret-santa/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/secret-santa/santaIcon.png", 
+            src: "santaIcon.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/secret-santa/santaIcon.png",
+            src: "santaIcon.png",
             sizes: "512x512",
             type: "image/png",
           },
